501ab5504ed5632e0ea8b10746d160584a124958
Joe Gaebel <joe.gaebel@hotmail.com>
2020-09-08 22:49:16 +1000

    First iteration of deploying heroku

---
+++ bin/deploy.sh
@@ -0,0 +1,19 @@
+set -e
+
+git remote add watch-with-dad-api https://git.heroku.com/watch-with-dad-api.git > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
+git remote add watch-with-dad-ui https://git.heroku.com/watch-with-dad-ui.git > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
+
+heroku buildpacks:add -a watch-with-dad-api https://github.com/lstoll/heroku-buildpack-monorepo -i 1 > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
+heroku buildpacks:add -a watch-with-dad-ui https://github.com/lstoll/heroku-buildpack-monorepo -i 1 > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
+heroku buildpacks:add -a watch-with-dad-ui mars/create-react-app -i 2 > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
+
+# Need to also add APP_BASE=ui and APP_BASE=api to the environment variables in heroku
+
+### UI
+git push watch-with-dad-ui master
+
+## API
+git push watch-with-dad-api master
+
+
+
