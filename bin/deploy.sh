337bcd1ec2d87b59239591834b3054ea2f2c1530
Joe Gaebel <joe.gaebel@hotmail.com>
2020-09-08 23:05:14 +1000

    Try nodejs buildpack

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,11 +5,12 @@ git remote add watch-with-dad-ui https://git.heroku.com/watch-with-dad-ui.git > 
 
 heroku buildpacks:add -a watch-with-dad-api https://github.com/lstoll/heroku-buildpack-monorepo -i 1 > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
 heroku buildpacks:add -a watch-with-dad-ui https://github.com/lstoll/heroku-buildpack-monorepo -i 1 > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
+
 heroku buildpacks:add -a watch-with-dad-ui mars/create-react-app -i 2 > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
+heroku buildpacks:add -a watch-with-dad-api heroku/nodejs -i 2 > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
 
-# Need to also add APP_BASE=ui and APP_BASE=api to the environment variables in heroku
-
-
+heroku config:set --app watch-with-dad-ui APP_BASE="ui"
+heroku config:set --app watch-with-dad-api APP_BASE="api"
 
 ### UI
 heroku config:set --app watch-with-dad-ui REACT_APP_BACKEND_URL="wss://watch-with-dad-api.herokuapp.com/"
