807589928be11c1f85e5bf9573ee067a8d213ec0
pivotal <pivotal@pivotal.io>
2020-09-13 23:44:31 +1000

    Add branch check to deploy script

--- bin/pcf-deploy.sh
+++ bin/pcf-deploy.sh
@@ -1,3 +1,11 @@
+branch_name=$(git symbolic-ref -q HEAD)
+
+if [ "$branch_name" != "refs/heads/master" ]
+then
+  echo "Can only deploy master, man."
+  exit 0
+fi
+
 cd ui
 REACT_APP_BACKEND_URL="wss://watch-with-dad-api.cfapps.io:4443" yarn build
 
