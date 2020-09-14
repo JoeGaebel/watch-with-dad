6d6e5772c443fd01221e172cdc7d4a701bc58b17
Joe Gaebel <joe.gaebel@hotmail.com>
2020-09-14 15:44:34 +1000

    Ensure subscribing only happens once

--- ui/src/hooks/useWebSocket.ts
+++ ui/src/hooks/useWebSocket.ts
@@ -2,25 +2,25 @@ import {Dispatch, MutableRefObject, useEffect} from "react";
 import {ConnectedToServerSuccessfully, ServerSocketEvent} from "../types/shared";
 
 export default function useWebSocket(connection: MutableRefObject<WebSocket>, dispatch: Dispatch<ServerSocketEvent>) {
-    function subscribeToSocketOpening() {
-        connection.current.onopen = () => {
-            const event = new ConnectedToServerSuccessfully()
-            dispatch(event)
-        }
-    }
-
-    function subscribeToSocketMessage() {
-        connection.current.onmessage = (event: MessageEvent) => {
-            try {
-                const parsedEvent = JSON.parse(event.data) as ServerSocketEvent
-                dispatch(parsedEvent)
-            } catch (error) {
+    useEffect(() => {
+        function subscribeToSocketOpening() {
+            connection.current.onopen = () => {
+                const event = new ConnectedToServerSuccessfully()
+                dispatch(event)
             }
         }
-    }
 
-    useEffect(() => {
+        function subscribeToSocketMessage() {
+            connection.current.onmessage = (event: MessageEvent) => {
+                try {
+                    const parsedEvent = JSON.parse(event.data) as ServerSocketEvent
+                    dispatch(parsedEvent)
+                } catch (error) {
+                }
+            }
+        }
+
         subscribeToSocketOpening();
         subscribeToSocketMessage();
-    })
+    }, [connection, dispatch])
 }
