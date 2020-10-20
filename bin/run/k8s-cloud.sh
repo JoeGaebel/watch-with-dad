47d6ef3e22dd1047f3e7bb57a480a95c90885a49
Joe Gaebel <joe.gaebel@hotmail.com>
2020-10-20 15:43:15 +1100

    Add revision history limit and record command for history

--- bin/run/k8s-cloud.sh
+++ bin/run/k8s-cloud.sh
@@ -2,6 +2,6 @@ set -e
 
 kubectl apply -f ./k8s/watch-with-dad-namespace.yml
 kubectl config set-context --current --namespace=watch-with-dad
-kubectl apply -f ./k8s -l type!=local
+kubectl apply -f ./k8s -l type!=local --record
 
 ./bin/run/force-restart.sh
