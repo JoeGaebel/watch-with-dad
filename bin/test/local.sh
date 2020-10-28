b3b36a60b493781f0c54db11f42815bb5f73819a
Joe Gaebel <joe.gaebel@hotmail.com>
2020-10-28 22:17:18 +1100

    Pull out local script

--- bin/test/local.sh
+++ bin/test/local.sh
@@ -18,24 +18,6 @@ killItByPort() {
     kill $(lsof -t -i:$1) > /dev/null 2>&1 || echo 0 > /dev/null 2>&1
 }
 
-function wait_for_port()
-{
-    local PORT=$1
-    while ! nc -z localhost $PORT &>/dev/null; do
-        sleep 0.5;
-        printf '.'
-    done
-}
-
-killItByPort 3000
-killItByPort 9090
-
-./bin/build/api-local.sh
-./bin/build/ui-local.sh
-
-(cd ui && yarn start | cat - &) > /dev/null 2>&1
-wait_for_port 3000
-(cd api && yarn start &) > /dev/null 2>&1
-wait_for_port 9090
+./bin/run/local.sh
 
 (cd e2e && yarn install && yarn test)
